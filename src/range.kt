023574610b236bd5eb985c77023b66ff180238f2
Firsoova08 <foxalishca@mail.ru>
2025-09-26 22:28:27 +0300

    5

---
+++ src/range.kt
@@ -0,0 +1,12 @@
+fun main() {
+//    val numbers = 1 ..  100
+//    val symbol = 'a'  ..  'z'
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    if (level in 1.. 50) {
+//        println("Уровень $level соответствует требованиям! Вы можете зайти в данж!")
+//    } else {
+//        println("Высокий уровень! Вход запрещён!")
+//
+//    }
+}
