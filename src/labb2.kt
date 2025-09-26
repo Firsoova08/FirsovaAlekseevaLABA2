1eb2729b7b6fe5c8db5e9d0614d8a0836ecd666d
Firsoova08 <foxalishca@mail.ru>
2025-09-26 22:21:13 +0300

    4

--- src/labb2.kt
+++ src/labb2.kt
@@ -1,14 +1,26 @@
 fun main() {
-    println("Hello world!")
-    print("Введите первое число: ")
-    val number1 = readln().toInt()
-    print("Введите второе число: ")
-    val number2 = readln().toInt()
-    if (number1 > number2) {
-        println("Число $number1 больше чем число $number2")
-    } else {
-        println("Число $number2 больше чем число $number1")
-    }
-    val max= if (number1 > number2) number1 else number2
-    println("Максимальное число -$max")
+//    println("Hello world!")
+//    print("Введите первое число: ")
+//    val number1 = readln().toInt()
+//    print("Введите второе число: ")
+//    val number2 = readln().toInt()
+//    if (number1 > number2) {
+//        println("Число $number1 больше чем число $number2")
+//    } else {
+//        println("Число $number2 больше чем число $number1")
+//    }
+//    val max= if (number1 > number2) number1 else number2
+//    println("Максимальное число -$max")
+//    val age = readln().toInt()
+//    val status = if (age >= 18) "Взрослый" else "Ребёнок"
+//    println(status)
+//    print("Введите ваш уровень: ")
+//    val level = readln().toInt()
+//    print("У вас есть VIP-статус? (true/false): ")
+//    val hasVIP = readln().toBoolean()
+//    if ((level >= 30 && hasVIP) || level >= 50) {
+//        println("Доступ разрешен!")
+//    } else {
+//        println("Доступ запрещен!")
+//    }
 }
