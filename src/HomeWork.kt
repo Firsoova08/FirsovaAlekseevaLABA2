f5c2fbb9190785eae83da727f58440523cf52cb3
Firsoova08 <foxalishca@mail.ru>
2025-09-26 23:08:58 +0300

    дз.2

--- src/HomeWork.kt
+++ src/HomeWork.kt
@@ -1,23 +1,35 @@
 fun main(){
     //1
-    print("Введите ваш уровень: ")
-    val num=readln().toInt()
-    val status: String
-    val bonus: Int
-    when{
-        num>10->{
-            status="Новичок"
-            bonus=10
-        }
-        num < 50->{
-            status="Опытный"
-            bonus=30
-        }
-     else ->{
-         status="Опытный"
-         bonus=100
-     }
-    }
-println("$status! Вы получаете +$bonus HP")
+//    print("Введите ваш уровень: ")
+//    val num=readln().toInt()
+//    val status: String
+//    val bonus: Int
+//    when{
+//        num<10->{
+//            status="Новичок"
+//            bonus=10
+//        }
+//        num < 50->{
+//            status="Опытный"
+//            bonus=30
+//        }
+//     else ->{
+//         status="Опытный"
+//         bonus=100
+//     }
+//    }
+//println("$status! Вы получаете +$bonus HP")
 
+    //2
+    print("Введите ваш возраст: ")
+   val age=readln().toInt()
+    if (age<12){
+        println("Вы ребенок")
+    }
+    else if (age<=17){
+        println("Вы подросток")
+    }
+    else{
+        println("Вы взрослый")
+    }
 }
